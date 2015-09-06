d1ebe9a112ab9e93c6d5447687f53834b16b3add
Kim Laï Trinh <kimlai@lrqdo.fr>
2015-09-06 21:46:36 +0200

    Add mark on navigation item to know what is currently playing.

--- components/Navigation.react.js
+++ components/Navigation.react.js
@@ -1,14 +1,38 @@
 var React = require('react');
 var Link = require('react-router').Link;
+var TracksModule = require('../modules/tracks');
+
+var reactor = require('../reactor');
+
+var getters = TracksModule.getters;
 
 module.exports = React.createClass({
+    mixins: [reactor.ReactMixin],
+
+    getDataBindings: function () {
+        return {
+            playlistId: getters.currentPlaylistId
+        };
+    },
+
     render: function () {
+        var navItems = [
+            { path: 'feed', displayName: 'Feed', playlistId: 'feed' },
+            { path: 'saved-tracks', displayName: 'Saved Tracks', playlistId: 'savedTracks' },
+            { path: 'published-tracks', displayName: 'Published Tracks', playlistId: 'publishedTracks' },
+        ];
+        var _self = this;
         return (
             <nav className="navigation">
                 <ul>
-                    <li><Link to="feed">Feed</Link></li>
-                    <li><Link to="saved-tracks">Saved Tracks</Link></li>
-                    <li><Link to="published-tracks">Published Tracks</Link></li>
+                    {navItems.map(function (navItem) {
+                        var className = (navItem.playlistId === _self.state.playlistId) ? "playing": "";
+                        return (
+                            <li key={navItem.path}>
+                                <Link className={className} to={navItem.path}>{navItem.displayName}</Link>
+                            </li>
+                        );
+                    })}
                 </ul>
             </nav>
         );
